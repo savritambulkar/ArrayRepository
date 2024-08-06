2fd9ceb230669555e7f16af2285d7ad358329911
savritambulkar <savritambulkar4@gmail.com>
2024-08-06 01:45:08 -0700

    Add files via upload

---
+++ second.java
@@ -0,0 +1,11 @@
+public class second {
+    public static void main(String[] args) {
+        int a[][]=new int[][]{{10,20,30,40},{50,60,40}};
+        for(int i=0;i<a.length;i++){
+
+            for(int j=0;j<a[i].length;j++){
+                System.out.println(a[i][j]);
+            }
+        }
+    }
+}
