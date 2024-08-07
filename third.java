544b4d7e0a941ccb86a4af3a8a1b4cfa9b45a9be
savritambulkar <savritambulkar4@gmail.com>
2024-08-06 21:58:43 -0700

    Add files via upload

---
+++ third.java
@@ -0,0 +1,11 @@
+public class third {
+    public static void main(String[] args) {
+        int a[]=new int[]{10,20,30,40};
+        int i=0;
+        do
+        {
+            System.out.println(a[i]);
+            i++;
+        } while (i<a.length);
+    }
+}
