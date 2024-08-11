bfabd1029bbf16465e8232f17da6a6870c4f249d
savritambulkar <savritambulkar4@gmail.com>
2024-08-11 08:48:51 -0700

    Add files via upload

---
+++ five.java
@@ -0,0 +1,18 @@
+public class five {
+    public static void main(String[] args) {
+        String a[]={"taruna","savri","kiran","Arati","priya","pinki"};
+        String temp;
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length-1;j++){
+                if(a[j].compareTo(a[j+1])>0){
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]+"  ");
+        }
+    }
+}
