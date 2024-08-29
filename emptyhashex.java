8a43dc11512dfeb35aeed1c9f1b9a9f7d8b4a6f7
savritambulkar <savritambulkar4@gmail.com>
2024-08-29 10:18:23 -0700

    Add files via upload

---
+++ emptyhashex.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class emptyhashex {
+    public static void main(String[] args) {
+        HashSet<Integer>he=new HashSet();
+        he.add(10);
+        he.add(20);
+        he.add(30);
+        he.add(40);
+        he.add(50);
+        he.add(60);
+        he.add(70);
+        he.add(80);
+        //System.out.println(he.isEmpty());
+      //  HashSet<Integer>h1=new HashSet(he);
+        //System.out.println(h1.isEmpty());
+        int a[]=new int[he.size()];
+       System.out.println(a.length);
+    }
+}
