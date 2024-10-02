9fcd28da8322898b4fc0fc2e1dec17c21400564e
savritambulkar <savritambulkar4@gmail.com>
2024-10-01 22:33:47 -0700

    Add files via upload

---
+++ _throws.java
@@ -0,0 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
+class _throws123 {
+    void readFile() throws FileNotFoundException
+    {
+        FileInputStream fis=new FileInputStream("./sav//.jpj");
+    }
+}
