035a94dc2739cd21a8d1c1538ff5578a5460b28e
savritambulkar <savritambulkar4@gmail.com>
2024-10-15 07:26:06 -0700

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,15 @@
+class Count {
+    public static void main(String[] args) {
+        String a="aaktyr";
+        int count =0;
+        for(int i=0;i<=a.length();i++){
+            
+            {
+                count++;
+            }
+           
+        }
+        System.out.println(count);
+        
+    }
+}
