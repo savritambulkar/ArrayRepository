11b764d3655526fbd9a25f21ac89cb7d7a02361b
savritambulkar <savritambulkar4@gmail.com>
2024-11-05 20:26:27 -0800

    Add files via upload

---
+++ _6Assg.java
@@ -0,0 +1,13 @@
+ class person {
+        String name;
+        int age;       
+}
+    class driver{
+        public static void main(String[] args) {
+         person ob=new person();
+            ob.name="Savri_Tambulkar";
+            ob.age=21;
+            System.out.println("name="+ob.name);
+        System.out.println("age:"+ob.age);
+               }
+    }
