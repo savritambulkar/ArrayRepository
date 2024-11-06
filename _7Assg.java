78d5f8e821f04f3c242da1824bc5bce526b013e3
savritambulkar <savritambulkar4@gmail.com>
2024-11-06 05:58:41 -0800

    Add files via upload

---
+++ _7Assg.java
@@ -0,0 +1,22 @@
+
+    class display {
+        public void Id()
+        {
+            String name="Savri Tambulkar" ;
+            int age =1;
+            System.out.println("name="+name);
+            System.out.println("age "+age);
+    }
+        } 
+        
+        class controler{
+            public static void main(String[] args) {
+             display ob = new display();
+                ob.Id(); 
+            
+               
+                   }
+        }
+    
+   
+
