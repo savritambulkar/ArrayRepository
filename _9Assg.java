7262aa5d3cb98f75299796bc61adc9b8082fd496
savritambulkar <savritambulkar4@gmail.com>
2024-11-08 07:04:05 -0800

    Add files via upload

---
+++ _9Assg.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+public class _9Assg {
+    public static void main(String[] args) {
+        _9Assg rw=new _9Assg();
+        try{
+            rw.readFile();
+
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+    }
+}
+class Test{
+    void readFile(){
+   // FileNotFoundException{
+     //
+}  
+}
