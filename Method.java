731cee74947f65072b21a3eb0a166e9036df3e1d
savritambulkar <savritambulkar4@gmail.com>
2024-11-16 10:45:50 -0800

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,10 @@
+class Method{
+    public static int addition(int a, int b)
+    {
+        int c;
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(addition(10,15));
+    }
+}
